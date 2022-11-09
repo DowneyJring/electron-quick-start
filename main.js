9a01359c947ba517877e4c44f1166aa5347c8f91
Gulu-Jing <64281564+DowneyJring@users.noreply.github.com>
2022-11-10 00:53:14 +0800

    API研究

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
+let progressInterval
+
 const createWindow = () => {
   const win = new BrowserWindow({
+    // 无边框
+    // frame: false,
+    // titleBarStyle: 'hidden',
+    // trafficLightPosition: { x: 10, y: 10 },
+    // titleBarOverlay: {
+    //   color: '#2f3241',
+    //   symbolColor: '#74b1be',
+    //   height: 60
+    // },
+    // 完全透明窗口
+    transparent: false,
     width: 800,
     height: 600,
     webPreferences: {
@@ -12,6 +25,23 @@ const createWindow = () => {
 
   ipcMain.handle('ping', () => 'pong')
   win.loadFile('index.html')
+
+  const INCREMENT = 0.03
+  const INTERVAL_DELAY = 100 // ms
+
+  let c = 0
+  progressInterval = setInterval(() => {
+    // update progress bar to next value
+    // values between 0 and 1 will show progress, >1 will show indeterminate or stick at 100%
+    win.setProgressBar(c)
+
+    // increment or reset progress bar
+    if (c < 2) {
+      c += INCREMENT
+    } else {
+      c = (-INCREMENT * 5) // reset to a bit less than 0 to show reset state
+    }
+  }, INTERVAL_DELAY)
 }
 
 app.whenReady().then(() => {
@@ -26,4 +56,9 @@ app.whenReady().then(() => {
 // 关闭所有窗口时退出应用 (Windows & Linux)
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
+})
+
+// before the app is terminated, clear both timers
+app.on('before-quit', () => {
+  clearInterval(progressInterval)
 })
--- preload.js
+++ preload.js
@@ -8,3 +8,16 @@ contextBridge.exposeInMainWorld('versions', {
   ping: () => ipcRenderer.invoke('ping'),
   // 能暴露的不仅仅是函数，我们还可以暴露变量
 })
+
+// All the Node.js APIs are available in the preload process.
+// 它拥有与Chrome扩展一样的沙盒
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+  // 访问 Node.js process.versions 对象，并运行一个基本的 replaceText 辅助函数将版本号插入到 HTML 文档中
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
